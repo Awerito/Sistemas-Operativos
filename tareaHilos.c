40acfedcb20e2a4d3c91a1ebef437bad8119c976
awerito <diegoignacio.munoz@alumnos.ulagos.cl>
2019-05-29 13:01:52 -0400

    Lista de procesos

--- tareaHilos.c
+++ tareaHilos.c
@@ -5,3 +5,110 @@
 // 	- Cuando un hilo se libera, se ocupa en un nuevo programa
 // 	- Indique la cantidad de tiempo total en terminar con todos los programas
 // 	- Indique la cantidad total de tiempo si no se hubieran usado hilos
+
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#define  TOTAL 6
+
+/* Node structure */
+typedef struct n {
+  //char *process, *user;
+  int time, memory, id;
+  struct n* next;
+}*node;
+
+/* isEmpty(queue); */
+bool isEmpty(node queue) {
+  return (queue == NULL);
+}
+
+/* showQueue(queue); */
+void showQueue(node queue) {
+  node aux = queue;
+
+  printf("PID\tMemory\tTime\n");
+  while (aux != NULL) {
+    printf("%d\t%d\t%d\n", aux->id, aux->memory, aux->time);
+    aux = aux->next;
+  }
+  printf("\n");
+}
+
+/* showData(queue); */
+int getTime(node queue) {
+  return queue->time;
+}
+
+/* getPID(queue); */
+int getMemory(node queue) {
+  return queue->memory;
+}
+
+/* getCount(queue); */
+int getID(node queue) {
+  return queue->id;
+}
+
+/* queue = push(queue, values); */
+node push(node queue, int id, int memory, int time) {
+  node aux = queue;
+
+  node newNode = (node) malloc(sizeof(struct n));
+  newNode->id = id;
+  newNode->memory = memory;
+  newNode->time = time;
+  newNode->next = NULL;
+
+  if (isEmpty(queue)) {
+    return newNode;
+  } else {
+    while (aux->next != NULL) {
+      aux = aux->next;
+    }
+    aux->next = newNode;
+    return queue;
+  }
+}
+
+/* queue = pop(queue); */
+node pop(node queue) {
+  if(!isEmpty(queue)){
+    if (queue->next == NULL) {
+      return NULL;
+    }
+    return queue->next;
+  }
+}
+
+void *inProcess(void *data) {
+  node p = (node) data;
+  fflush(stdout);
+  sleep(p->time);
+  printf("Process %d finish in %d seconds\n", p->id, p->time);
+}
+
+int main(int argc, char const *argv[]) {
+  srand(time(NULL));
+  node tasks;
+
+  for (size_t i = 0; i < TOTAL; i++) {
+    int ram = rand() % 1048576 + 1;
+    int req = rand() % 5 + 1;
+    tasks = push(tasks, i, ram, req);
+  }
+
+  showQueue(tasks);
+
+  //pthread_t thread1, thread2, thread3, thread4;
+
+  // pthread_create(&thread1, NULL, inProcess, (void *) &task1);
+  // pthread_join(thread1, NULL);
+  // printf("\n");
+  return 0;
+}
